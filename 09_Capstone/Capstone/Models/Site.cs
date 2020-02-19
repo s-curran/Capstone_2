5b4a8a078da380ba18081a195769f303eb57436f
Lisa <lisalynn103187@gmail.com>
2020-02-19 14:38:58 -0500

    Added Site and Reservation classes

---
+++ 09_Capstone/Capstone/Models/Site.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Capstone.Models
+{
+    public class Site
+    {
+        public int SiteId { get; set; }
+
+        public int CampgroundId { get; set; }
+
+        public int SiteNumber { get; set; }
+
+        public int MaxOccupancy { get; set; }
+
+        public bool IsAccessible { get; set; }
+
+        public int MaxRVLength { get; set; }
+
+        public bool HasUtilities { get; set; }
+    }
+}
