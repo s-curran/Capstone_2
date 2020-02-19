e96f409f6bf4efc2afbd6a0fbbbb4d70a834728b
Sean Curran <scurran3@kent.edu>
2020-02-19 14:39:10 -0500

    2 classes added, park and campground

---
+++ 09_Capstone/Capstone/Models/Campground.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Capstone.Models
+{
+    public class Campground
+    {
+        public int CampgroundId { get; set; }
+        public int ParkId { get; set; }
+        public string Name { get; set; }
+        public DateTime OpenFrom { get; set; }
+        public DateTime OpenTo { get; set; }
+        public decimal DailyFee { get; set; }
+
+    }
+}
