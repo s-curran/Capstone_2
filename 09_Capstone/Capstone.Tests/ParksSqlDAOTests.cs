faff4c8d45abc623ca1fb691b9ac44be20d044a8
Sean Curran <scurran3@kent.edu>
2020-02-20 10:46:59 -0500

    first test written

--- 09_Capstone/Capstone.Tests/ParksSqlDAOTests.cs
+++ 09_Capstone/Capstone.Tests/ParksSqlDAOTests.cs
@@ -1,13 +1,67 @@
+using Capstone.DAL;
+using Capstone.Models;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.IO;
+using System.Transactions;
 
 namespace Capstone.Tests
 {
     [TestClass]
     public class ParksSqlDAOTests
     {
+        private TransactionScope transaction = null;
+        private string connectionString = "Server=.\\SqlExpress;Database=NPCampground;Trusted_Connection=True;";
+        private int lastParkId;
+
+        [TestInitialize]
+        public void SetupDatabase()
+        {
+            // Start a transaction, so we can roll back when we are finished with this test
+            transaction = new TransactionScope();
+
+            // Open Setup.Sql and read in the script to be executed
+            string setupSQL;
+            using (StreamReader rdr = new StreamReader("Setup.sql"))
+            {
+                setupSQL = rdr.ReadToEnd();
+            }
+
+            // Connect to the DB and execute the script
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                conn.Open();
+
+                SqlCommand cmd = new SqlCommand(setupSQL, conn);
+                SqlDataReader rdr = cmd.ExecuteReader();
+
+                while (rdr.Read())
+                {
+                    lastParkId = Convert.ToInt32(rdr["lastPark"]);
+                }
+            }
+        }
+
+
+        [TestCleanup]
+        public void CleanupDatabase()
+        {
+            // Rollback the transaction to get our good data back
+            transaction.Dispose();
+        }
         [TestMethod]
         public void GetAllParksTest()
         {
+            // Arrange
+            ParkSqlDAO dao = new ParkSqlDAO(connectionString);
+
+            // Act
+            IList<Park> actual = dao.GetAllParks();
+
+            // Assert 
+            Assert.AreEqual(2, actual.Count);
 
         }
     }
